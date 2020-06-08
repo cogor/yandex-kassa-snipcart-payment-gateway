0093a76ee8a3cbad1b9b3bb398cd2f67cbdc7e1a
Ilya Artamonov <ilya.sosidka@gmail.com>
2020-06-08 18:17:08 +0300

    update

--- index.js
+++ index.js
@@ -4,36 +4,15 @@ const YandexCheckout = require("yandex-checkout")(
 );
 const axios = require("axios");
 const express = require("express");
-var pino = require("express-pino-logger")();
+const pino = require("express-pino-logger")();
+const bodyParser = require("body-parser");
 const app = express();
 const port = 1234;
 app.use(pino);
+app.use(bodyParser.json());
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
-
-// YandexCheckout.createPayment(
-//   {
-//     amount: {
-//       value: "2.00",
-//       currency: "RUB",
-//     },
-//     payment_method_data: {
-//       type: "bank_card",
-//     },
-//     confirmation: {
-//       type: "redirect",
-//       return_url: "https://www.merchant-website.com/return_url",
-//     },
-//   }
-// )
-//   .then(function (result) {
-//     console.log({ payment: result });
-//   })
-//   .catch(function (err) {
-//     console.error(err);
-//   });
-// Get public token from query string
 
 app.get("/api/", (req, res) => res.send("Hello World!"));
 app.post("/api/methods", (req, res) => {
